5a1dfa2d05066c5be6cdf0f2d0c20a90ccf2adb6
Ash-spector <shuklaaniket090@gmail.com>
2026-02-02 19:00:25 +0530

    Add the logic behind view and controller button

---
+++ Mvc_Project Db/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Project_Db.Data;
+
+namespace Mvc_Project_Db.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbcontext context;
+
+        public EmployeeController()
+        {
+            context = new ApplicationDbcontext();
+        }
+
+        public ActionResult Index()
+        {
+            var employeelist = context.Employees.ToList();
+            return View(employeelist);
+        }
+    }
+}
