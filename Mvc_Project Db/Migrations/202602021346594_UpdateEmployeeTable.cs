357fc05772e333e2ef691342f3d3491905082376
Ash-spector <shuklaaniket090@gmail.com>
2026-02-02 19:19:35 +0530

    Initial commit

---
+++ Mvc_Project Db/Migrations/202602021346594_UpdateEmployeeTable.cs
@@ -0,0 +1,27 @@
+﻿namespace Mvc_Project_Db.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateEmployeeTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CourseName = c.String(),
+                        CourseDuration = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Courses");
+        }
+    }
+}
